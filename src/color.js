6ceeb03ae94aaf47f6fb6d27f5c21fbbdb17660f
Johnny Wu <KarasAya@gmail.com>
2014-06-03 17:45:05 +0800

    add css export functions

--- src/color.js
+++ src/color.js
@@ -24,6 +24,49 @@ FIRE.Color = (function () {
         ;
     };
 
+    Color.prototype.toCSS = function ( opt ) {
+        if ( opt === 'rgba' ) {
+            return "rgba(" + 
+                Math.floor(this.r * 255) + "," + 
+                Math.floor(this.g * 255) + "," + 
+                Math.floor(this.b * 255) + "," + 
+                this.a.toFixed(2) + ")"
+            ;
+        }
+        else if ( opt === 'rgb' ) {
+            return "rgb(" + 
+                Math.floor(this.r * 255) + "," + 
+                Math.floor(this.g * 255) + "," + 
+                Math.floor(this.b * 255) + ")"
+            ;
+        }
+        else if ( opt === '#rgb' ) {
+            // TODO
+            return "";
+        }
+        else if ( opt === '#rrggbb' ) {
+            // TODO
+            return "";
+        }
+        return "";
+    };
+
+    Color.prototype.equalTo = function ( rhs ) {
+        if ( !(rhs instanceof FIRE.Color) )
+            return false;
+
+        if ( this.r !== rhs.r )
+            return false;
+        if ( this.g !== rhs.g )
+            return false;
+        if ( this.b !== rhs.b )
+            return false;
+        if ( this.a !== rhs.a )
+            return false;
+
+        return true;
+    };
+
     return Color;
 })();
 
