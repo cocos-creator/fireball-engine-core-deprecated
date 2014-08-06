b7dbbf5fcc8e10b8c03a6ab00534a883ec4ba2ba
Jare Guo <jare85@163.com>
2014-08-06 15:41:32 +0800

    support nodejs context

--- src/editor/platform.js
+++ src/editor/platform.js
@@ -1,9 +1,10 @@
 //
-FIRE.isnode = !!(typeof(process) !== 'undefined' && process.versions && process.versions.node);
-FIRE.isnw = !!(FIRE.isnode && 'node-webkit' in process.versions);   // node-webkit
-FIRE.isas = !!(FIRE.isnode && 'atom-shell' in process.versions);    // atom-shell
-FIRE.isapp = FIRE.isnw || FIRE.isas;                                // native client
-FIRE.isweb = !FIRE.isnode && !FIRE.isapp;                           // common web browser
+FIRE.isnode = !!(typeof process !== 'undefined' && process.versions && process.versions.node);
+FIRE.isviewctx = (typeof __dirname === 'undefined' || __dirname === null);  // window's render context in node-webkit or atom-shell
+FIRE.isnw = !!(FIRE.isnode && 'node-webkit' in process.versions);           // node-webkit
+FIRE.isas = !!(FIRE.isnode && 'atom-shell' in process.versions);            // atom-shell
+FIRE.isapp = FIRE.isnw || FIRE.isas;                                        // native client
+FIRE.isweb = !FIRE.isnode && !FIRE.isapp;                                   // common web browser
 
 if (FIRE.isnode) {
     FIRE.isdarwin = process.platform === 'darwin';
@@ -88,68 +89,167 @@ FIRE.Path.setExtension = function (path, newExtension) {
     return path.substring(0, dotIndex) + newExtension;
 };
 
-if (FIRE.isnw) {
-    FIRE.getSavePath = function (defaultFilename, preserveDirKey, callback) {
-        var persistentId = 'SaveFileDialog';
-        var chooser = document.getElementById(persistentId);
-        if (chooser) {
-            // remove the old one or change event may not fired
-            document.body.removeChild(chooser);
-        }
-        chooser = document.createElement('input');
-        document.body.appendChild(chooser);
-        chooser.id = persistentId;
-        chooser.style.display = 'none';
-        chooser.type = 'file';
-
-        chooser.nwsaveas = defaultFilename;
-        var defaultDir = localStorage[preserveDirKey];
-        chooser.nwworkingdir = defaultDir || '';
-        
-        chooser.onchange = function (evt) {
-            document.body.removeChild(chooser);
-            //chooser.removeEventListener("change", arguments.callee);
-            chooser.onchange = null;
-            //console.log('value ' + this.value);
-            localStorage[preserveDirKey] = this.value;
-            callback(this.value);
-        };
-        chooser.click();
-    };
-    var nwgui = require('nw.gui');
-    FIRE.showItemInFolder = nwgui.Shell.showItemInFolder;
-}
-else if (FIRE.isas) {
-    FIRE.getSavePath = function (defaultFilename, preserveDirKey, callback, title, browserWindow) {
-        var defaultDir = localStorage[preserveDirKey];
-        var defaultPath = null;
-        if (defaultDir && typeof defaultDir === 'string') {
-            defaultDir = FIRE.Path.dirname(defaultDir);
-            defaultPath = FIRE.Path.join(defaultDir, defaultFilename);
-        }
-        var remote = require('remote');
-        var dialog = remote.require('dialog');
-        var options = {
-            title: title,
-            defaultPath: defaultPath,
-        };
-        if (!browserWindow) {
-            browserWindow = remote.getCurrentWindow();
-        }
-        dialog.showSaveDialog(browserWindow, options, function (path) {
-            if (path) {
-                localStorage[preserveDirKey] = path;
+if (FIRE.isviewctx) {
+    if (FIRE.isnw) {
+        FIRE.getSavePath = function (defaultFilename, preserveDirKey, callback) {
+            var persistentId = 'SaveFileDialog';
+            var chooser = document.getElementById(persistentId);
+            if (chooser) {
+                // remove the old one or change event may not fired
+                document.body.removeChild(chooser);
             }
-            callback(path);
-        });
-    };
-    var shell = require('shell');
-    FIRE.showItemInFolder = shell.showItemInFolder;
-}
-else {
-    var error = function () {
-        throw "This function can only be used in node-webkit or atom-shell";
-    };
-    FIRE.getSavePath = error;
-    FIRE.showItemInFolder = error;
+            chooser = document.createElement('input');
+            document.body.appendChild(chooser);
+            chooser.id = persistentId;
+            chooser.style.display = 'none';
+            chooser.type = 'file';
+
+            chooser.nwsaveas = defaultFilename;
+            var defaultDir = localStorage[preserveDirKey];
+            chooser.nwworkingdir = defaultDir || '';
+        
+            chooser.onchange = function (evt) {
+                document.body.removeChild(chooser);
+                //chooser.removeEventListener("change", arguments.callee);
+                chooser.onchange = null;
+                //console.log('value ' + this.value);
+                localStorage[preserveDirKey] = this.value;
+                callback(this.value);
+            };
+
+            //chooser.addEventListener("change", function (evt) {
+            //    console.log('change');
+            //}, false);
+            //chooser.addEventListener("blur", function (evt) {
+            //    console.log('blur');
+            //}, false);
+            //chooser.addEventListener("load", function (evt) {
+            //    console.log('load');
+            //}, false);
+            //chooser.addEventListener("unload", function (evt) {
+            //    console.log('unload');
+            //}, false);
+            //chooser.addEventListener("abort", function (evt) {
+            //    console.log('abort');
+            //}, false);
+            //chooser.addEventListener("click", function (evt) {
+            //    console.log('click');
+            //}, false);
+            //chooser.addEventListener("error", function (evt) {
+            //    console.log('error');
+            //}, false);
+            //chooser.addEventListener("resize", function (evt) {
+            //    console.log('resize');
+            //}, false);
+            //chooser.addEventListener("select", function (evt) {
+            //    console.log('select');
+            //}, false);
+            //chooser.addEventListener("submit", function (evt) {
+            //    console.log('submit');
+            //}, false);
+            //chooser.addEventListener("reset", function (evt) {
+            //    console.log('reset');
+            //}, false);
+            //chooser.addEventListener("focus", function (evt) {
+            //    console.log('focus');
+            //}, false);
+            //chooser.addEventListener("focusin", function (evt) {
+            //    console.log('focusin');
+            //}, false);
+            //chooser.addEventListener("focusout", function (evt) {
+            //    console.log('focusout');
+            //}, false);
+            //chooser.addEventListener("error", function (evt) {
+            //    console.log('error');
+            //}, false);
+
+
+            //chooser.addEventListener("change", function (evt) {
+            //    console.log('change');
+            //}, true);
+            //chooser.addEventListener("blur", function (evt) {
+            //    console.log('blur');
+            //}, true);
+            //chooser.addEventListener("load", function (evt) {
+            //    console.log('load');
+            //}, true);
+            //chooser.addEventListener("unload", function (evt) {
+            //    console.log('unload');
+            //}, true);
+            //chooser.addEventListener("abort", function (evt) {
+            //    console.log('abort');
+            //}, true);
+            //chooser.addEventListener("click", function (evt) {
+            //    console.log('click');
+            //}, true);
+            //chooser.addEventListener("error", function (evt) {
+            //    console.log('error');
+            //}, true);
+            //chooser.addEventListener("resize", function (evt) {
+            //    console.log('resize');
+            //}, true);
+            //chooser.addEventListener("select", function (evt) {
+            //    console.log('select');
+            //}, true);
+            //chooser.addEventListener("submit", function (evt) {
+            //    console.log('submit');
+            //}, true);
+            //chooser.addEventListener("reset", function (evt) {
+            //    console.log('reset');
+            //}, true);
+            //chooser.addEventListener("focus", function (evt) {
+            //    console.log('focus');
+            //}, true);
+            //chooser.addEventListener("focusin", function (evt) {
+            //    console.log('focusin');
+            //}, true);
+            //chooser.addEventListener("focusout", function (evt) {
+            //    console.log('focusout');
+            //}, true);
+            //chooser.addEventListener("error", function (evt) {
+            //    console.log('error');
+            //}, true);
+            chooser.click();
+            //chooser.value = '';
+            //chooser.files = new FileList();
+            //chooser.files.append(new File('',''));
+            //chooser.files = null;
+        };
+        var nwgui = require('nw.gui');
+        FIRE.showItemInFolder = nwgui.Shell.showItemInFolder;
+    }
+    else if (FIRE.isas) {
+        FIRE.getSavePath = function (defaultFilename, preserveDirKey, callback, title, browserWindow) {
+            var defaultDir = localStorage[preserveDirKey];
+            var defaultPath = null;
+            if (defaultDir && typeof defaultDir === 'string') {
+                defaultDir = FIRE.Path.dirname(defaultDir);
+                defaultPath = FIRE.Path.join(defaultDir, defaultFilename);
+            }
+            var remote = require('remote');
+            var dialog = remote.require('dialog');
+            var options = {
+                title: title,
+                defaultPath: defaultPath,
+            };
+            if (!browserWindow) {
+                browserWindow = remote.getCurrentWindow();
+            }
+            dialog.showSaveDialog(browserWindow, options, function (path) {
+                if (path) {
+                    localStorage[preserveDirKey] = path;
+                }
+                callback(path);
+            });
+        };
+        var shell = require('shell');
+        FIRE.showItemInFolder = shell.showItemInFolder;
+    }
+    else {
+        var error = function () {
+            throw "This function can only be used in node-webkit or atom-shell";
+        };
+        FIRE.getSavePath = error;
+        FIRE.showItemInFolder = error;
+    }
 }
