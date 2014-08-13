8a1e26952066d2890b666ebd53887329e1b74b26
Johnny Wu <KarasAya@gmail.com>
2014-08-13 12:13:36 +0800

    move some dom function from core to editor-ui

--- src/editor/dom.js
+++ src/editor/dom.js
@@ -1,79 +1,6 @@
 if (typeof window !== 'undefined') {
 
-
-var _findInChildren = function ( element, elementToFind ) {
-    for ( var i = 0; i < element.children.length; ++i ) {
-        var childEL = element.children[i];
-        if ( childEL === elementToFind )
-            return true;
-
-        if ( childEL.children.length > 0 )
-            if ( _findInChildren( childEL, elementToFind ) )
-                return true;
-    }
-    return false;
-};
-
-//
-FIRE.find = function ( elements, elementToFind ) {
-    if ( Array.isArray(elements) || 
-         elements instanceof NodeList ||
-         elements instanceof HTMLCollection ) 
-    {
-        for ( var i = 0; i < elements.length; ++i ) {
-            var element = elements[i];
-            if ( element === elementToFind )
-                return true;
-
-            if ( _findInChildren ( element, elementToFind ) )
-                return true;
-        }
-        return false;
-    }
-
-    // if this is a single element
-    if ( elements === elementToFind )
-        return true;
-
-    return _findInChildren( elements, elementToFind );
-};
-
-//
-FIRE.getParentTabIndex = function ( element ) {
-    var parent = element.parentElement;
-    while ( parent ) {
-        if ( parent.tabIndex !== null && 
-             parent.tabIndex !== undefined &&
-             parent.tabIndex !== -1 )
-            return parent.tabIndex;
-
-        parent = parent.parentElement;
-    }
-    return 0;
-};
-
-FIRE.getFirstFocusableChild = function ( element ) {
-    if ( element.tabIndex !== null && 
-         element.tabIndex !== undefined &&
-         element.tabIndex !== -1 )
-    {
-        return element;
-    }
-
-    var el = null;
-    for ( var i = 0; i < element.children.length; ++i ) {
-        el = FIRE.getFirstFocusableChild(element.children[i]);
-        if ( el !== null )
-            return el;
-    }
-    if ( element.shadowRoot ) {
-        el = FIRE.getFirstFocusableChild(element.shadowRoot);
-        if ( el !== null )
-            return el;
-    }
-
-    return null;
-};
+// TODO: move these functions to fireball-x/editor/utils
 
 //
 FIRE.getTrimRect = function (img, trimThreshold) {
@@ -94,33 +21,6 @@ FIRE.getTrimRect = function (img, trimThreshold) {
 
     // get trim
     return _doGetTrimRect(pixelBuffer, img.width, img.height, trimThreshold);
-};
-
-var _dragGhost = null; 
-FIRE.addDragGhost = function ( cursor ) {
-    // add drag-ghost
-    if ( _dragGhost === null ) {
-        _dragGhost = document.createElement('div');
-        _dragGhost.classList.add('drag-ghost');
-        _dragGhost.style.position = 'fixed';
-        _dragGhost.style.zIndex = '999';
-        _dragGhost.style.left = '0';
-        _dragGhost.style.top = '0';
-        _dragGhost.style.width = window.innerWidth + 'px';
-        _dragGhost.style.height = window.innerHeight + 'px';
-        _dragGhost.oncontextmenu = function() { return false; };
-    }
-    _dragGhost.style.cursor = cursor;
-    document.body.appendChild(_dragGhost);
-};
-
-FIRE.removeDragGhost = function () {
-    if ( _dragGhost !== null ) {
-        _dragGhost.style.cursor = 'auto';
-        if ( _dragGhost.parentNode !== null ) {
-            _dragGhost.parentNode.removeChild(_dragGhost);
-        }
-    }
 };
 
 var _readDir = function (dirReader, callback) {
