15ad8ca95c7d73e3931faef76f740a555da4c6db
Johnny Wu <KarasAya@gmail.com>
2014-09-02 13:16:31 +0800

    update gulpfiles

--- gulpfile.js
+++ gulpfile.js
@@ -137,6 +137,7 @@ gulp.task('ref', function() {
 gulp.task('watch', function() {
     gulp.watch(paths.src, ['min']).on( 'error', gutil.log );
 });
+gulp.task('watch-self', ['watch'] );
 
 //
 gulp.task('all', ['min', 'test', 'ref'] );
