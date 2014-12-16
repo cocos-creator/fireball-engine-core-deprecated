945723df4453683cf8d2d7263392be368981598e
Johnny Wu <KarasAya@gmail.com>
2014-12-17 01:40:07 +0800

    add dirty flag

--- src/definition.js
+++ src/definition.js
@@ -30,7 +30,8 @@ else {
 var Destroyed = 1 << 0;
 var ToDestroy = 1 << 1;
 var DontSave = 1 << 2;
-var EditorOnly  = 1 << 3;       // dont save in build
+var EditorOnly  = 1 << 3; // dont save in build
+var Dirty  = 1 << 4; // used in editor        
 
 var ObjectFlags = {
     DontSave: DontSave,
