47d272fddc824550412bf9fb1acfc79fea549281
Johnny Wu <KarasAya@gmail.com>
2014-12-26 10:11:10 +0800

    change sprite and sprite importer for the new import pipeline

--- src/asset/sprite.js
+++ src/asset/sprite.js
@@ -7,7 +7,6 @@ Fire.Sprite = (function () {
         Sprite.$super.call(this);
 
         if (img) {
-            this.rawTexture = new Fire.Texture(img);
             this.texture = new Fire.Texture(img);
             this.width = img.width;
             this.height = img.height;
@@ -15,23 +14,23 @@ Fire.Sprite = (function () {
     });
 
     // basic settings
-    Sprite.prop('rawTexture', null, Fire.ObjectType(Fire.Texture), Fire.Tooltip('untrimmed raw texture'), Fire.EditorOnly);
     Sprite.prop('texture', null, Fire.ObjectType(Fire.Texture), Fire.Tooltip('texture to render'));
     Sprite.prop('rotated', false);
-    Sprite.prop('trim', false, Fire.EditorOnly);
-    Sprite.prop('trimThreshold', 1, Fire.EditorOnly);
     Sprite.prop('pivot', new Fire.Vec2(0.5, 0.5), Fire.Tooltip('The pivot is normalized, like a percentage.\n' +
                                                                '(0,0) means the bottom-left corner and (1,1) means the top-right corner.\n' +
                                                                'But you can use values higher than (1,1) and lower than (0,0) too.'));
 
-    // trims
-    Sprite.prop('trimX', 0, Fire.Integer);
-    Sprite.prop('trimY', 0, Fire.Integer);
+    // raw texture info (used for texture-offset calculation)
+    Sprite.prop('trimX', 0, Fire.Integer); // offset of the sprite in raw-texture
+    Sprite.prop('trimY', 0, Fire.Integer); // offset of the sprite in raw-texture
+    Sprite.prop('rawWidth', 0, Fire.Integer);
+    Sprite.prop('rawHeight', 0, Fire.Integer);
+
+    // trimmed info
+    Sprite.prop('x', 0, Fire.Integer); // uv of the sprite in atlas-texture
+    Sprite.prop('y', 0, Fire.Integer); // uv of the sprite in atlas-texture
     Sprite.prop('width', 0, Fire.Integer, Fire.Tooltip('trimmed width'));
     Sprite.prop('height', 0, Fire.Integer, Fire.Tooltip('trimmed height'));
-    Sprite.prop('x', 0, Fire.Integer);
-    Sprite.prop('y', 0, Fire.Integer);
-    //
 
     Object.defineProperty(Sprite.prototype, 'rotatedWidth', {
         get: function () { return this.rotated ? this.height : this.width; }
