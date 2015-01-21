f2189169f247ba8c5fea4cf88eeed02c2254b874
knoxHuang <821686174@qq.com>
2015-01-21 11:47:56 +0800

    add BitmapFont

---
+++ src/asset/bitmap-font.js
@@ -0,0 +1,42 @@
+﻿
+var BitmapFont = (function () {
+
+    var CharInfo = {
+        id: -1,
+        trim_x: 0,
+        trim_y: 0,
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+        xOffset: 0,
+        yOffset: 0,
+        xAdvance: 0,
+        rotated: false,
+    };
+
+    var Kerning = {
+        first: 0,
+        second: 0,
+        amount: 0,
+    };
+
+    var BitmapFont = Fire.define("Fire.BitmapFont", Fire.Asset, function () {
+        Fire.Asset.call(this);
+    });
+
+    BitmapFont.CharInfo = CharInfo;
+    BitmapFont.Kerning = Kerning;
+
+    BitmapFont.prop('texture', null, Fire.ObjectType(Fire.Texture));
+    BitmapFont.prop('charInfos', []);
+    BitmapFont.prop('kernings', []);
+    BitmapFont.prop('baseLine', 0, Fire.Integer);
+    BitmapFont.prop('lineHeight', 0, Fire.Integer);
+    BitmapFont.prop('size', 0, Fire.Integer);
+    BitmapFont.prop('face', null, Fire.HideInInspector);
+
+    return BitmapFont;
+})();
+
+Fire.BitmapFont = BitmapFont;
---
+++ src/asset/text-asset.js
@@ -0,0 +1,9 @@
+﻿Fire.TextAsset = (function () {
+    var TextAsset = Fire.define("Fire.TextAsset", Fire.Asset, function () {
+        Fire.Asset.call(this);
+    });
+
+    TextAsset.prop('text', '', Fire.HostType('text'));
+
+    return TextAsset;
+})();
