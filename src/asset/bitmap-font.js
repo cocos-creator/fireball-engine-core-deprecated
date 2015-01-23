598d11918cea649562a00d05330850c10d7acf97
knoxHuang <821686174@qq.com>
2015-01-23 11:05:22 +0800

    update bitmap-font

--- src/asset/bitmap-font.js
+++ src/asset/bitmap-font.js
@@ -1,32 +1,27 @@
 ﻿
 var BitmapFont = (function () {
 
-    var CharInfo = {
-        id: -1,
-        trim_x: 0,
-        trim_y: 0,
-        x: 0,
-        y: 0,
-        width: 0,
-        height: 0,
-        xOffset: 0,
-        yOffset: 0,
-        xAdvance: 0,
-        rotated: false,
-    };
+    //var CharInfo = {
+    //    id: -1,
+    //    trim_x: 0,
+    //    trim_y: 0,
+    //    x: 0,
+    //    y: 0,
+    //    width: 0,
+    //    height: 0,
+    //    xOffset: 0,
+    //    yOffset: 0,
+    //    xAdvance: 0,
+    //    rotated: false,
+    //};
 
-    var Kerning = {
-        first: 0,
-        second: 0,
-        amount: 0,
-    };
+    //var Kerning = {
+    //    first: 0,
+    //    second: 0,
+    //    amount: 0,
+    //};
 
-    var BitmapFont = Fire.define("Fire.BitmapFont", Fire.Asset, function () {
-        Fire.Asset.call(this);
-    });
-
-    BitmapFont.CharInfo = CharInfo;
-    BitmapFont.Kerning = Kerning;
+    var BitmapFont = Fire.define("Fire.BitmapFont", Fire.Asset, null);
 
     BitmapFont.prop('texture', null, Fire.ObjectType(Fire.Texture));
     BitmapFont.prop('charInfos', []);
