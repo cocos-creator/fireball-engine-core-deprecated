fbd13dd1998dca9d4c3060999df1f4090d771a38
pandamicro <lphuabin@gmail.com>
2015-03-11 18:28:14 +0800

    Add Color#toCCColor

--- src/color.js
+++ src/color.js
@@ -113,6 +113,15 @@ var Color = (function () {
         return (this.r * 255 << 16) + (this.g * 255 << 8) + this.b * 255;
     };
 
+    Color.prototype.toCCColor = function () {
+        return {
+            r: this.r * 255,
+            g: this.g * 255,
+            b: this.b * 255,
+            a: this.a * 255
+        };
+    };
+
     Color.prototype.fromHSV = function ( h, s, v ) {
         var rgb = Fire.hsv2rgb( h, s, v );
         this.r = rgb.r;
