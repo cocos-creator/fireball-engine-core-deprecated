b324fb62819abe43b09acdb732158a25512a63af
knoxHuang <821686174@qq.com>
2015-04-23 18:00:37 +0800

    update fireball-x/dev#594:实现获取 texture 像素值

--- src/asset/texture.js
+++ src/asset/texture.js
@@ -93,5 +93,58 @@ Fire.Texture = (function () {
     //    this.height = this.image.height;
     //};
 
+    /**
+     * Returns pixel color at coordinates (x, y).
+     *
+     * If the pixel coordinates are out of bounds (larger than width/height or small than 0),
+     * they will be clamped or repeated based on the texture's wrap mode.
+     *
+     * @method getPixel
+     * @param {number} x
+     * @param {nmber} y
+     * @return {Fire.Color}
+     */
+    Texture.prototype.getPixel = function (x, y) {
+        if (!canvasCtxToGetPixel) {
+            var canvas = document.createElement('canvas');
+            canvas.width = 1;
+            canvas.height = 1;
+            canvasCtxToGetPixel = canvas.getContext('2d');
+        }
+        if (this.wrapMode === Texture.WrapMode.Clamp) {
+            x = Math.clamp(x, 0, this.image.width);
+            y = Math.clamp(y, 0, this.image.height);
+        }
+        else if (this.wrapMode === Texture.WrapMode.Repeat) {
+            x = x % this.image.width;
+            if (x < 0) {
+                x += this.image.width
+            }
+            y = y % this.image.width;
+            if (y < 0) {
+                y += this.image.width
+            }
+        }
+        canvasCtxToGetPixel.clearRect(0, 0, 1, 1);
+        canvasCtxToGetPixel.drawImage(this.image, x, y, 1, 1, 0, 0, 1, 1);
+
+        var imgBytes = null;
+        try {
+            imgBytes = canvasCtxToGetPixel.getImageData(0, 0, 1, 1).data;
+        }
+        catch (e) {
+            Fire.error("An error has occurred. This is most likely due to security restrictions on reading canvas pixel data with local or cross-domain images.");
+            return Fire.Color.transparent;
+        }
+        var result = new Fire.Color();
+        result.r = imgBytes[0] / 255;
+        result.g = imgBytes[1] / 255;
+        result.b = imgBytes[2] / 255;
+        result.a = imgBytes[3] / 255;
+        return result;
+    }
+
     return Texture;
 })();
+
+var canvasCtxToGetPixel = null;
