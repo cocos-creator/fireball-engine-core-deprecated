c826bee05aebc998d385be55458f1c4bb74e755d
Jare Guo <jare85@163.com>
2015-05-07 16:33:26 +0800

    make JsonAsset.json hide in inspector

--- src/__intro.js
+++ src/__intro.js
@@ -2,10 +2,19 @@
     var root = this;
     /**
      * !#en
-     * Global object with classes, properties and methods you can access from anywhere.
-     * See [methods and properties](../classes/Fire.html).
+     * Global object with runtime classes, properties and methods you can access from anywhere.
+     *
+     * Submodules:
+     * - [JS](./Fire.JS.html)
+     * - [Spine](./Fire.Spine.html)
+     *
      * !#zh
      * 可全局访问的公共方法和属性，也会包括一些组件和类的静态方法
+     *
+     * 包含的子模块:
+     * - [JS](./Fire.JS.html)
+     * - [Spine](./Fire.Spine.html)
+     *
      * @module Fire
      * @main Fire
      */
--- src/asset/json-asset.js
+++ src/asset/json-asset.js
@@ -1,9 +1,13 @@
-﻿var JsonAsset = (function () {
-
-    var JsonAsset = Fire.extend('Fire.JsonAsset', Asset)
-                        .prop('json', null, Fire.RawType('json'));
-
-    return JsonAsset;
-})();
+﻿var JsonAsset = Fire.Class({
+    name: 'Fire.JsonAsset',
+    extends: Asset,
+    properties: {
+        json: {
+            default: null,
+            rawType: 'json',
+            visible: false      // 显示出来会很卡
+        }
+    }
+});
 
 Fire.JsonAsset = JsonAsset;
